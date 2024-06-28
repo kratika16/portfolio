842a6fdeef0fda74d8eda4edc38164c3bdfdd042
Kratika Yadav <yadavkratika57@gmail.com>
2024-06-28 15:16:37 +0530

    Added dynamic contact methods

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 import "./LandingPage.css";
 import Home from "./Home";
 import Transition from "./Transition";
+import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function MainLandingPage() {
   const [transitionComplete, setTransitionComplete] = useState(false);
+  const contact = useSelector((state) => state.portfolio.contact);
 
   const handleTransitionComplete = () => {
     setTransitionComplete(true);
@@ -18,6 +23,20 @@ function MainLandingPage() {
       {transitionComplete && (
         <div>
           <Home />
+          <div className=" rounded-lg flex flex-row justify-center w-full space-x-4 fixed bottom-0">
+        <a href={`tel:${contact.phone}`} className="text-2xl">
+          <FontAwesomeIcon icon={faPhone} />
+        </a>
+        <a href={`mailto:${contact.email}`} className="text-2xl">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+        <a href={contact.linkedin} target="_blank" rel="noopener noreferrer" className="text-2xl">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a href={contact.github} target="_blank" rel="noopener noreferrer" className="text-2xl">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+      </div>
         </div>
       )}
     </div>
